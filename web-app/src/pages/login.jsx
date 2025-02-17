ee3181924db33dcdb9257f4a94adb195a7ba07cf
Attawat kammas <attawat.k@kkumail.com>
2025-02-18 01:42:55 +0700

    update login

--- web-app/src/pages/login.jsx
+++ web-app/src/pages/login.jsx
@@ -4,6 +4,7 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth, db } from "../firebase";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import 'animate.css';
 
 const Login = () => {
     const navigate = useNavigate();
@@ -43,23 +44,25 @@ const Login = () => {
                 height: "100vh",
             }}
         >
-<Card 
-    elevation={3} 
-    sx={{ 
-        padding: 4, 
-        borderRadius: 2, 
-        textAlign: "center", 
-        width: "100%",
-        maxWidth: 400,
-        boxShadow: "0px 4px 10px rgba(1, 1, 1, 0.5)",
-        "&:hover": {
-            transform: "scale(1.05)",
-            boxShadow: "0px 6px 12px rgba(0, 0, 0, 0.4)",
-        }
-    }}
->
+            <Card
+                elevation={3}
+                sx={{
+                    backgroundColor: "#19a05e",
+                    padding: 4,
+                    borderRadius: 2,
+                    border: "4px solid #fbc02d",
+                    textAlign: "center",
+                    width: "100%",
+                    maxWidth: 400,
+                    boxShadow: "0px 4px 10px rgba(1, 1, 1, 0.5)",
+                    "&:hover": {
+                        transform: "scale(1.05)",
+                        boxShadow: "0px 6px 12px rgba(0, 0, 0, 0.4)",
+                    }
+                }}
+            >
 
-            
+
                 <Typography variant="h4" fontWeight="bold" gutterBottom>
                     ระบบจัดการห้องเรียน
                 </Typography>
@@ -67,28 +70,31 @@ const Login = () => {
                     ลงชื่อเข้าใช้เพื่อจัดการห้องเรียนของคุณ
                 </Typography>
                 <Box mt={3}>
-                <Button
-    variant="contained"
-    color="primary"
-    onClick={handleLogin}
-    sx={{ 
-        padding: "10px 20px", 
-        fontSize: "16px",
-        transition: "0.3s",
-        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.3)",
-        "&:hover": {
-            backgroundColor: "green",
-            transform: "scale(1.05)",
-            boxShadow: "0px 6px 12px rgba(0, 0, 0, 0.4)",
-        }
-    }}
->
-    Sign in with Google
-</Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleLogin}
+                        sx={{
+                            padding: "10px 20px",
+                            fontSize: "16px",
+                            transition: "0.3s",
+                            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.3)",
+                            "&:hover": {
+                                color:"green",    
+                                backgroundColor: "#FFFFFF",
+                                transform: "scale(1.05)",
+                                boxShadow: "0px 6px 12px rgba(0, 0, 0, 0.4)",
+                            }
+                        }}
+                    >
+                        Sign in with Google
+                    </Button>
 
                 </Box>
             </Card>
+            
         </Container>
+        
     );
 };
 
